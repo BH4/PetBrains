fb1b6b9dc2e28b96092fc93d2d240fc228d618b7
BH4 <BH4@users.noreply.github.com>
2015-07-22 17:53:30 -0500

    Working
    double commit...

--- brain.py
+++ brain.py
@@ -9,6 +9,7 @@ def randomSyn(nInput,nOutput,nHiddenLayers,numPerHiddenLayer):
 
     return syn
 
+"""
 def mutate(syn):
     numMutate=int(totSyn*mutationRate)
 
@@ -24,7 +25,11 @@ def mutate(syn):
         syn[n][r][c]=2*np.random.random()-1
 
     return syn
-
+"""
+def mutate(num):
+    if np.random.random()<mutationRate:
+        return num+(2*np.random.random()-1)
+    return num
 
 
 class brain:
@@ -41,44 +46,48 @@ class brain:
         return curr
 
     def breed(self,other):
-        #one position crossover
+        #one position crossover//changed to random at every position
         #pick random position to switch from first brain to second
         #all breeding brains must be the same size
         numLayers=len(self.syn)
 
         #choose a layer
-        n=np.random.randint(0,numLayers)
+        #n=np.random.randint(0,numLayers)
         #choose a row
-        r=np.random.randint(0,len(self.syn[n]))
+        #r=np.random.randint(0,len(self.syn[n]))
         #choose a collumn
-        c=np.random.randint(0,len(self.syn[n][r]))
+        #c=np.random.randint(0,len(self.syn[n][r]))
 
         newSyn=[]
         for i in xrange(numLayers):
+            """
             if i<n:
                 newSyn.append(self.syn[i])
             elif i>n:
                 newSyn.append(other.syn[i])
             else:
-                newlayer=[]
-                for j in xrange(len(self.syn[i])):
-                    if j<r:
-                        newlayer.append(self.syn[i][j])
-                    elif j>r:
-                        newlayer.append(other.syn[i][j])
+            """
+            newlayer=[]
+            for j in xrange(len(self.syn[i])):
+                """
+                if j<r:
+                    newlayer.append(self.syn[i][j])
+                elif j>r:
+                    newlayer.append(other.syn[i][j])
+                else:
+                """
+                newrow=[]
+                for k in xrange(len(self.syn[i][j])):
+                    if np.random.random()<=.5:
+                        newrow.append(mutate(self.syn[i][j][k]))
                     else:
-                        newrow=[]
-                        for k in xrange(len(self.syn[i][j])):
-                            if k<=c:
-                                newrow.append(self.syn[i][j][k])
-                            else:
-                                newrow.append(other.syn[i][j][k])
-                        newlayer.append(newrow)
+                        newrow.append(mutate(other.syn[i][j][k]))
+                newlayer.append(newrow)
 
-                newSyn.append(np.array(newlayer))
-
+            newSyn.append(np.array(newlayer))
+        
         #mutation
-        newSyn=mutate(newSyn)
+        #newSyn=mutate(newSyn)
         
         newbrain=brain(4,4,1,5)
         newbrain.syn=newSyn
